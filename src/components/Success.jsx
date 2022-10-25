1a5a12f503f1a1b3c4266d5624c83cb3f4e22661
varun <varunrana829@gmail.com>
2022-10-25 18:52:31 +0530

    first commit

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -10,7 +10,7 @@ function Success() {
 		<>
 			{success && (
 				<p className="text-green-600 bg-green-50 border-l border-l-green-600 py-3 px-2">
-					you have guess the correct answer : {values.correctAnswer}
+					You have guess the correct answer : {values.correctAnswer}
 				</p>
 			)}
 		</>
